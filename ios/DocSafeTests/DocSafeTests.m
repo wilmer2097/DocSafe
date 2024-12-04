7dbf1011f16b5c07fbe82351aaf004148b6706ce
wilmer2097 <wilmer20acero@gmail.com>
2024-12-04 18:33:54 -0500

    cambio

--- ios/DocSafeTests/DocSafeTests.m
+++ ios/DocSafeTests/DocSafeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface luffyTests : XCTestCase
+@interface DocSafeTests : XCTestCase
 
 @end
 
-@implementation luffyTests
+@implementation DocSafeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
